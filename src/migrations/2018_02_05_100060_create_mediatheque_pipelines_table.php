7e437db31edf9747652582203f2deb6dc75f2c28
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2021-01-17 23:48:37 -0500

    Fix with float column

--- src/migrations/2018_02_05_100060_create_mediatheque_pipelines_table.php
+++ src/migrations/2018_02_05_100060_create_mediatheque_pipelines_table.php
@@ -22,7 +22,7 @@ class CreateMediathequePipelinesTable extends Migration
             $table->boolean('ended')->default(false);
             $table->boolean('failed')->default(false);
             $table->text('failed_exception')->nullable();
-            $table->float('progress', 1, 8)->default(0);
+            $table->float('progress', 8, 1)->default(0);
             $table->dateTime('started_at')->nullable();
             $table->dateTime('ended_at')->nullable();
             $table->timestamps();
