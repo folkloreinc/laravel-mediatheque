b7d5ae052e1f3951ed70854e8b1083e37780c788
Nicolas Roy B <phatshambler@gmail.com>
2022-09-20 10:30:57 -0400

    Color extractor update

--- src/Folklore/Mediatheque/Services/ColorExtractor.php
+++ src/Folklore/Mediatheque/Services/ColorExtractor.php
@@ -4,24 +4,40 @@ namespace Folklore\Mediatheque\Services;
 
 use Folklore\Mediatheque\Contracts\Services\Color as ColorService;
 use Folklore\Mediatheque\Contracts\Services\Palette as PaletteService;
-use League\ColorExtractor\Color;
-use League\ColorExtractor\ColorExtractor as BaseColorExtractor;
-use League\ColorExtractor\Palette;
+use ColorThief\ColorThief;
 use Exception;
 
 class ColorExtractor implements ColorService, PaletteService
 {
+    protected $format = 'hex';
+
+    protected $quality = 10;
+
+    /**
+     * Get the dominant color of a path
+     * @param  string $path The path of a file
+     * @return mixed The color
+     */
+    public function getDominantColor(string $path)
+    {
+        try {
+            $palette = ColorThief::getColor($path, $this->quality, null, $this->format);
+            return $palette;
+        } catch (Exception $e) {
+            return null;
+        }
+    }
+
     /**
      * Get the colors of a path
      * @param  string $path The path of a file
-     * @param  int $path The path of a file
+     * @param  int $count The count
      * @return array The colors
      */
     public function getColors(string $path, int $count = 1): ?array
     {
         try {
-            $palette = Palette::fromFilename($path);
-            return $this->getHex($palette->getMostUsedColors($count));
+            $this->getPalette($path, $count);
         } catch (Exception $e) {
             return null;
         }
@@ -30,26 +46,26 @@ class ColorExtractor implements ColorService, PaletteService
     /**
      * Get the color palette of a path
      * @param  string $path The path of a file
-     * @param  int $path The path of a file
+     * @param  int $count The count
      * @return array The palette
      */
     public function getPalette(string $path, int $count = 1): ?array
     {
         try {
-            $palette = Palette::fromFilename($path);
-            $extractor = new BaseColorExtractor($palette);
-            return $this->getHex($extractor->extract($count));
+            $palette = ColorThief::getPalette($path, $count, $this->quality, null, $this->format);
+            return $palette;
         } catch (Exception $e) {
             return null;
         }
     }
 
-    protected function getHex($colors)
+    public function setFormat(string $format)
     {
-        return collect($colors)
-            ->map(function ($color) {
-                return Color::fromIntToHex($color);
-            })
-            ->toArray();
+        $this->format = $format; // rgb, hex, int, obj
+    }
+
+    public function setQuality(int $quality)
+    {
+        $this->quality = $quality; // 1 to 10
     }
 }
